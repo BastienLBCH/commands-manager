96a6e22eca87fd723e27cda6cfa60645967231c0
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2024-06-26 00:28:42 +0200

    basic subfolder system working

--- src/views/mainview.rs
+++ src/views/mainview.rs
@@ -1,3 +1,5 @@
+use egui::Color32;
+
 use crate::models::app::{MyApp, Section};
 use std::env;
 use std::process::Command;
@@ -24,10 +26,30 @@ fn execute_command(cmd_args: Vec<String>) {
     }
 }
 
-fn display_section(ui: &mut egui::Ui, section: &mut Section, depth: i8) {
+fn generate_string_from_depth(text_to_write: &str, depth: usize) -> String {
+    let mut generated_string: String = String::from("-".repeat(depth as usize));
+    generated_string.push_str(" ");
+    generated_string.push_str(text_to_write);
+    generated_string
+}
+
+fn display_section(ui: &mut egui::Ui, section: &mut Section, depth: u8) {
+    let base_color: u8 = 232;
+    let depth_multiplier: u8 = 16;
+    let rgb_intensity = base_color - (depth_multiplier * depth);
+    let rgb_intensity = rgb_intensity;
     let button = ui.add_sized(
         [ui.available_width(), 40.],
-        egui::Button::new(section.name.clone()).wrap(true),
+        egui::Button::new(generate_string_from_depth(
+            section.name.clone().as_str(),
+            depth as usize,
+        ))
+        .fill(Color32::from_rgb(
+            rgb_intensity,
+            rgb_intensity,
+            rgb_intensity,
+        ))
+        .wrap(true),
     );
     if button.clicked() {
         section.visible = !section.visible;
@@ -39,7 +61,13 @@ fn display_section(ui: &mut egui::Ui, section: &mut Section, depth: i8) {
             }
         }
         for ssh_instruction in &section.ssh_instructions {
-            if ui.label(ssh_instruction.name.clone()).double_clicked() {
+            if ui
+                .label(generate_string_from_depth(
+                    ssh_instruction.name.clone().as_str(),
+                    depth as usize,
+                ))
+                .double_clicked()
+            {
                 execute_command(ssh_instruction.command.clone());
             };
         }
