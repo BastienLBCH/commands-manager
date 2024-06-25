fccf68725c6ae1cbab53da96e34c93a9a8683371
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2024-06-25 23:30:20 +0200

    Recursively displaying nested sections

--- src/views/mainview.rs
+++ src/views/mainview.rs
@@ -1,7 +1,51 @@
-use crate::models::app::MyApp;
+use crate::models::app::{MyApp, Section};
 use std::env;
 use std::process::Command;
 // use eframe::egui::{Button, FontId};
+
+fn execute_command(cmd_args: Vec<String>) {
+    let mut args: Vec<String> = Vec::new();
+    let shell_to_use: String;
+    if env::consts::OS == "windows" {
+        args.push(String::from("/C"));
+        args.append(&mut cmd_args.clone());
+        shell_to_use = String::from("cmd");
+        Command::new(shell_to_use)
+            .args(args)
+            .spawn()
+            .expect("Failed to execute command");
+    } else {
+        args.push(String::from("-c"));
+        args.append(&mut cmd_args.clone());
+        shell_to_use = String::from("sh");
+        Command::new(shell_to_use)
+            .args(args)
+            .spawn()
+            .expect("Failed to execute command");
+    }
+}
+
+fn display_section(ui: &mut egui::Ui, section: &mut Section) {
+    let button = ui.add_sized(
+        [ui.available_width(), 40.],
+        egui::Button::new(section.name.clone()).wrap(true),
+    );
+    if button.clicked() {
+        section.visible = !section.visible;
+    }
+    if section.visible {
+        if section.subsections.len() > 0 {
+            for subsection in &mut section.subsections {
+                display_section(ui, subsection);
+            }
+        }
+        for ssh_instruction in &section.ssh_instructions {
+            if ui.label(ssh_instruction.name.clone()).double_clicked() {
+                execute_command(ssh_instruction.command.clone());
+            };
+        }
+    }
+}
 
 pub fn show_home_page(app: &mut MyApp, ui: &mut egui::Ui) {
     app.load_configuration();
@@ -16,38 +60,7 @@ pub fn show_home_page(app: &mut MyApp, ui: &mut egui::Ui) {
             egui::ScrollArea::vertical().show(ui, |ui| {
                 ui.set_min_size(ui.available_size());
                 for section in &mut app.sections {
-                    let button = ui.add_sized(
-                        [ui.available_width(), 40.],
-                        egui::Button::new(section.name.clone()).wrap(true),
-                    );
-                    if button.clicked() {
-                        section.visible = !section.visible;
-                    }
-                    if section.visible {
-                        for ssh_instruction in &section.ssh_instructions {
-                            if ui.label(ssh_instruction.name.clone()).double_clicked() {
-                                let mut args: Vec<String> = vec![];
-                                let shell_to_use: String;
-                                if env::consts::OS == "windows" {
-                                    args.push(String::from("/C"));
-                                    args.append(&mut ssh_instruction.command.clone());
-                                    shell_to_use = String::from("cmd");
-                                    Command::new(shell_to_use)
-                                        .args(args)
-                                        .spawn()
-                                        .expect("Failed to execute command");
-                                } else {
-                                    args.push(String::from("-c"));
-                                    args.append(&mut ssh_instruction.command.clone());
-                                    shell_to_use = String::from("sh");
-                                    Command::new(shell_to_use)
-                                        .args(args)
-                                        .spawn()
-                                        .expect("Failed to execute command");
-                                }
-                            };
-                        }
-                    }
+                    display_section(ui, section);
                 }
             });
         },
